836bf63e746d0f2ff995bb293be55458581b3021
Rothezee <rothalan83@gmail.com>
2025-04-26 15:39:52 -0300

    Delete Ejercicio2 directory

--- Ejercicio2/main.js
+++
@@ -1,8 +0,0 @@
-let boton = document.getElementById("boton1");
-boton.addEventListener("click", function() {
-    if(boton.textContent === "pulsar"){
-        boton.textContent = "pulsado";
-    }else{
-        boton.textContent = "pulsar";
-    }
-});
